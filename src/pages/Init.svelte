d8d9c51f1abd016705576f7da2ea583d78c5c7fc
Ray Feng <ryfprivate@gmail.com>
2024-07-11 04:02:08 +1000

    Added prettier formatter, fixed folder aliases

--- src/pages/Init.svelte
+++ src/pages/Init.svelte
@@ -1,10 +1,10 @@
-<script>
-    import {gameIsReady} from "../lib/helpers/LoLApi.svelte.ts";
-    $effect(() => {
-        gameIsReady();
-    })
+<script lang="ts">
+  import { gameIsReady } from "$lib/helpers/LoLApi.svelte";
+  $effect(() => {
+    gameIsReady();
+  });
 </script>
 
 <div class="size-full bg-blue-200">
-    <h1>Init</h1>
+  <h1>Init</h1>
 </div>
