c6b1294819895cedde2f46b2f7f257bfb18e705b
Ray Feng <ryfprivate@gmail.com>
2024-07-11 03:35:20 +1000

    Added 16:9 resolution base

--- src/App.svelte
+++ src/App.svelte
@@ -4,9 +4,22 @@
 </script>
 
 <main>
-  <Router {routes} />
+  <div class="content">
+    <Router {routes} />
+  </div>
 </main>
 
 <style>
-  
+  main {
+    position: relative;
+    /* Magic css number for enforcing 16:9 aspect ratio */
+    padding-bottom: 56.25%;
+    overflow: hidden;
+  }
+  .content {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    max-height: 100vh;
+  }
 </style>
--- src/routes/Init.svelte
+++ src/routes/Init.svelte
@@ -1,3 +1,3 @@
-<div class="h-screen w-screen bg-blue-200">
+<div class="size-full bg-blue-200">
     <h1>Init</h1>
 </div>
