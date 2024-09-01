62c21e471246dadae41af58aa155a087d73a7ac5
Ray Feng <ryfprivate@gmail.com>
2024-09-02 00:24:47 +1000

    Added new title screen ocean and sky visuals

--- src/lib/components/visual/scenery/SkyOcean.svelte
+++ src/lib/components/visual/scenery/SkyOcean.svelte
@@ -1,3 +1,7 @@
+<script>
+  import ocean from "$assets/ocean_waves.jpg";
+</script>
+
 <div id="scenery">
   <div class="sky-container">
     <div class="sun"></div>
@@ -7,21 +11,38 @@
     <div class="sun-reflection">
       <div class="sun"></div>
     </div>
+    <img id="ocean-image" src={ocean} alt="ocean" />
+    <svg>
+      <filter id="turbulence" x="0" y="0" width="100%" height="100%">
+        <feTurbulence
+          id="ocean-filter"
+          numOctaves="3"
+          seed="2"
+          baseFrequency="0.02 0.05"
+        />
+        <feDisplacementMap scale="20" in="SourceGraphic" />
+        <animate
+          xlink:href="#ocean-filter"
+          attributeName="baseFrequency"
+          dur="22s"
+          keyTimes="0;1"
+          values="0.02 0.06;0.04 0.08"
+          repeatCount="indefinite"
+        />
+      </filter>
+    </svg>
   </div>
 </div>
 
 <style>
   #scenery {
-    /* --v1: #be91c6; */
     --v1: #61d0cf;
-    /* --v2: #4bc7cf; */
     --v2: #12a1c0;
     --v3: #5e30d9;
     --transparentv3: #5e30d900;
     --v4: #3b1895;
-    /* --s1: #fea798; */
     --s1: #efeebc;
-    --s2: #fee154;
+    --s2: #e8ed92;
     --cloud: #fea798;
 
     position: absolute;
@@ -40,16 +61,18 @@
   }
   .sun {
     top: 100%;
-    right: 44%;
+    left: 50%;
+    translate: -50% 0;
 
-    background: white;
+    background: #ffe;
     border-radius: 50%;
     width: 88px;
     height: 88px;
-    box-shadow: 0px 0px 11px 11px white;
+    box-shadow: 0px 0px 11px 11px #ffe;
 
     transform: translate3d(0, 0, 0);
-    animation: rise 11s infinite;
+    animation: rise 11s;
+    animation-fill-mode: forwards;
   }
   .ambient {
     width: 100%;
@@ -57,29 +80,36 @@
     background: radial-gradient(circle at 50% 100%, var(--s2), transparent);
     mix-blend-mode: color-burn;
 
-    animation: fade 11s infinite;
+    animation: fade 11s;
+    animation-fill-mode: forwards;
   }
   @keyframes fade {
-    24% {
-      opacity: 1;
-    }
-    66%,
-    88% {
+    0% {
       opacity: 0;
+    }
+    88% {
+      opacity: 1;
     }
   }
   .ocean-container {
     bottom: 0;
     width: 100%;
     height: 50%;
-    background: linear-gradient(transparent, var(--v2));
+    background: linear-gradient(#98b0fe55, var(--v2));
     box-shadow: inset 0 1px 4px -3px white;
+  }
+  #ocean-image {
+    left: -1%;
+    max-width: none;
+    width: 102%;
+    height: 100%;
+    opacity: 0.1;
+    filter: url(#turbulence);
   }
   .sun-reflection {
     width: 100%;
     height: 100%;
     background: radial-gradient(circle at 50% 0%, var(--s2), transparent);
-    animation: fade 11s infinite;
   }
   .sun-reflection .sun {
     top: 0;
--- src/pages/Title.svelte
+++ src/pages/Title.svelte
@@ -1,16 +1,25 @@
 <script>
   import { SkyOcean } from "$components/visual/scenery";
+  import sub from "$assets/sprites/sub.png";
   import { getLolApi } from "$apis/lol.svelte";
   const lolApi = getLolApi();
 </script>
 
 <div id="page_title" class="size-full">
-  <div id="ui" class="relative size-full flex justify-center">
-    <h1 class="text-title text-6xl font-bold mt-24 p-4">
+  <div id="ui" class="relative size-full flex flex-col items-center">
+    <h1 class="text-title text-6xl font-bold mt-24">
       {lolApi.getText("title")}
     </h1>
+    <p class="text-title text-4xl font-bold p-4">
+      {lolApi.getText("subtitle")}
+    </p>
   </div>
   <SkyOcean />
+  <div
+    class="absolute left-1/2 -translate-x-1/2 bottom-[22%] w-[111px] h-[111px] overflow-hidden"
+  >
+    <img id="sub-image" src={sub} alt="sub" class="absolute bottom-[-44%]" />
+  </div>
 </div>
 
 <style>
@@ -22,5 +31,19 @@
   }
   .text-title {
     text-shadow: 1px 1px 2px black;
+  }
+  #sub-image {
+    animation: bob 11s linear infinite;
+  }
+  @keyframes bob {
+    0% {
+      transform: rotate(0);
+    }
+    50% {
+      transform: rotate(-8deg);
+    }
+    100% {
+      transform: rotate(0);
+    }
   }
 </style>
