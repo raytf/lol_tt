f47847e6755cebe6086d1ab4277fc899d1be1d3f
Ray Feng <ryfprivate@gmail.com>
2024-10-31 15:08:01 +1100

    Added dynamic id to turbulent image (multiple turbulent images)

--- src/lib/components/ui/img/TurbulentImg.svelte
+++ src/lib/components/ui/img/TurbulentImg.svelte
@@ -4,16 +4,21 @@
     minFrequency = [0.02, 0.06],
     maxFrequency = [0.04, 0.08],
     duration = 11,
+    scale = 20,
     yoyo = false,
     class: extraClass,
   }: {
     src: string;
     duration?: number;
+    scale?: number;
     minFrequency?: number[];
     maxFrequency?: number[];
     yoyo?: boolean;
-    class: string;
+    class?: string;
   } = $props();
+
+  const randId = Math.floor(Math.random() * 100);
+  console.log("random", randId);
 
   const freqKeyTimes = `0;${yoyo ? "0.5;" : ""}1`;
   const freqValues = `${minFrequency[0]} ${minFrequency[1]};${maxFrequency[0]} ${maxFrequency[1]}${
@@ -21,18 +26,23 @@
   }`;
 </script>
 
-<img {src} alt="" class="image-turbulent {extraClass}" />
+<img
+  {src}
+  alt=""
+  class="image-turbulent {extraClass}"
+  style="filter: url(#turbulence-{randId});"
+/>
 <svg>
-  <filter id="turbulence" x="0" y="0" width="100%" height="100%">
+  <filter id="turbulence-{randId}" x="0" y="0" width="100%" height="100%">
     <feTurbulence
-      id="ocean-filter"
+      id="ocean-filter-{randId}"
       numOctaves="3"
       seed="2"
-      baseFrequency="0.02 0.05"
+      baseFrequency="{minFrequency[0]} {minFrequency[1]}"
     />
-    <feDisplacementMap scale="20" in="SourceGraphic" />
+    <feDisplacementMap {scale} in="SourceGraphic" />
     <animate
-      xlink:href="#ocean-filter"
+      xlink:href="#ocean-filter-{randId}"
       attributeName="baseFrequency"
       dur="{duration}s"
       keyTimes={freqKeyTimes}
@@ -50,7 +60,6 @@
     max-width: none;
     width: 102%;
     height: 102%;
-    filter: url(#turbulence);
     pointer-events: none;
 
     will-change: transform;
