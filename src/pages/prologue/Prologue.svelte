a5d9d1d7ec5edcb6beb1942783ff7912f012e9ba
Ray Feng <ryfprivate@gmail.com>
2024-11-17 19:32:49 +1100

    Exaggerated shadow in prologue

--- src/pages/prologue/Prologue.svelte
+++ src/pages/prologue/Prologue.svelte
@@ -1,6 +1,5 @@
 <script>
   import { onMount } from "svelte";
-  import { fade, blur } from "svelte/transition";
   import { gsap } from "gsap";
   import { TextOverlay } from "$components/text";
   import { Blackdrop } from "$components/ui";
@@ -16,13 +15,12 @@
   ];
   let currentSequence = $state(0);
   let blackdropOpacity = $state(100);
-  let shadowOpacity = $state(0);
 
   function nextSequence() {
     currentSequence += 1;
     if (currentSequence == 1) {
       blackdropOpacity = 22;
-      shadowOpacity = 88;
+      tlShadowAppear();
     }
     if (currentSequence >= textSequence.length) {
       $audioApi.stopTrack({ src: "music/tritons-triangle.mp3" });
@@ -32,9 +30,19 @@
     }
   }
 
+  function tlShadowAppear() {
+    gsap.to(".shadow", {
+      opacity: 0.88,
+      scale: 1.5,
+      duration: 4,
+      force3D: false,
+    });
+  }
+
   onMount(() => {
     blackdropOpacity = 44;
     gsap.to("#pg-prologue_bg", { scale: 1.4, duration: 44, force3D: false });
+    gsap.set(".shadow", { opacity: 0 });
     $audioApi.playTrack({
       src: "music/tritons-triangle.mp3",
       volume: 0.66,
@@ -57,7 +65,6 @@
       src={shadow}
       alt="shadow"
       class="shadow absolute size-full blur-[14px] z-[1]"
-      style="opacity: {shadowOpacity}%"
     />
     <img src={ship} alt="ship" class="anim-bob absolute size-full z-[2]" />
   </div>
@@ -76,8 +83,5 @@
 <style>
   .anim-bob {
     animation: bob 44s cubic-bezier(0.25, 0.46, 0.45, 0.94) infinite;
-  }
-  .shadow {
-    transition: opacity 11s;
   }
 </style>
