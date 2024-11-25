7f8e8a51d7c01b1bfb58d343bfff5d1a5af76f99
Ray Feng <ryfprivate@gmail.com>
2024-11-25 23:45:01 +1000

    Added animated arrows

---
+++ src/lib/components/svg/icons/animated/ArrowLeft.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  let {
+    startTime = 0.5,
+    pathDuration = 0.2,
+    class: extraClass,
+  }: { startTime?: number; pathDuration?: number; class?: string } = $props();
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="1em"
+  height="1em"
+  viewBox="0 0 24 24"
+  ><g
+    fill="none"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    ><path stroke-dasharray="20" stroke-dashoffset="20" d="M21 12h-17.5"
+      ><animate
+        fill="freeze"
+        attributeName="stroke-dashoffset"
+        begin="{startTime}s"
+        dur="{pathDuration}s"
+        values="20;0"
+      /></path
+    ><path stroke-dasharray="12" stroke-dashoffset="12" d="M3 12l7 7M3 12l7 -7"
+      ><animate
+        fill="freeze"
+        attributeName="stroke-dashoffset"
+        begin="{startTime ? startTime + pathDuration : pathDuration}s"
+        dur="{pathDuration}s"
+        values="12;0"
+      /></path
+    ></g
+  ></svg
+>
---
+++ src/lib/components/svg/icons/animated/index.ts
@@ -0,0 +1,6 @@
+import ArrowUp from "./ArrowUp.svelte";
+import ArrowDown from "./ArrowDown.svelte";
+import ArrowLeft from "./ArrowLeft.svelte";
+import ArrowRight from "./ArrowRight.svelte";
+import Pulse from "./Pulse.svelte";
+export { ArrowUp, ArrowDown, ArrowLeft, ArrowRight, Pulse };
